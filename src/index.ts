4b517e1f3acfe8cc1c7f636b542e53d91c1baafe
Shantanu Singh <shannformal323@gmail.com>
2025-03-03 08:21:21 +0530

    feat: added products router

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express from 'express'
+import productsRoutes from './routes/products'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use('/products', productsRoutes)
 
 app.listen(3000, () => {
   console.log('server running on port 3000...')
